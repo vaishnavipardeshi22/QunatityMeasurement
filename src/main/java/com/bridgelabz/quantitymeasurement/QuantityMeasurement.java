e9a377249b2981c079d83e57be1f4f2014c1e73e
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-04-09 09:30:04 +0530

    ADDED : QuantityMeasurement main class

---
+++ src/main/java/com/bridgelabz/quantitymeasurement/QuantityMeasurement.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.quantitymeasurement;
+
+public class QuantityMeasurement {
+    public static void main(String[] args) {
+        System.out.println(" ******************** WELCOME TO QUANTITY MEASUREMENT COMPUTATION ******************** ");
+    }
+}
