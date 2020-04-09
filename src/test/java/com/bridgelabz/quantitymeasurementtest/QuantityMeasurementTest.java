ba9a3da5d86224e17861529754fcbbccfed68192
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-04-09 09:28:43 +0530

    ADDED : Object to create QuantityMeasurement main Class

---
+++ src/test/java/com/bridgelabz/quantitymeasurementtest/QuantityMeasurementTest.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.quantitymeasurementtest;
+
+import com.bridgelabz.quantitymeasurement.QuantityMeasurement;
+
+public class QuantityMeasurementTest {
+    QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
+}
