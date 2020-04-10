b817f0086f572d8371024b46cfb0c21b1a4fb419
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-04-10 09:12:34 +0530

    ADDED : Test method 1.1 to check feet value is equal to expected value

--- src/test/java/com/bridgelabz/quantitymeasurementtest/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/quantitymeasurementtest/QuantityMeasurementTest.java
@@ -1,7 +1,16 @@
 package com.bridgelabz.quantitymeasurementtest;
 
 import com.bridgelabz.quantitymeasurement.QuantityMeasurement;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class QuantityMeasurementTest {
+
     QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
+
+    @Test
+    public void givenFeet_ShouldEqual() {
+        double actualFeetValue = quantityMeasurement.getFeetValue(1.0);
+        Assert.assertEquals(1.0, actualFeetValue, 0.0);
+    }
 }
