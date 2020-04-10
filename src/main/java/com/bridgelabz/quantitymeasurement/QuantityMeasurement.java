fffb93c863e5987ece1dd2254fa9ede6b6ecc3ee
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-04-10 09:13:11 +0530

    ADDED : Function to get feet value

--- src/main/java/com/bridgelabz/quantitymeasurement/QuantityMeasurement.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/QuantityMeasurement.java
@@ -1,7 +1,11 @@
 package com.bridgelabz.quantitymeasurement;
 
 public class QuantityMeasurement {
-    public static void main(String[] args) {
-        System.out.println(" ******************** WELCOME TO QUANTITY MEASUREMENT COMPUTATION ******************** ");
+
+    private double feetValue;
+
+    public double getFeetValue(double feetValue) {
+        this.feetValue = feetValue;
+        return feetValue;
     }
 }
