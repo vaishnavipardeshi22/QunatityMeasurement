7341ef27d44ddce64808737303302a621138ac08
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-04-11 10:24:11 +0530

    ADDED : Enum type for feet to yard conversion

--- src/main/java/com/bridgelabz/quantitymeasurement/Unit.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/Unit.java
@@ -2,7 +2,8 @@ package com.bridgelabz.quantitymeasurement;
 
 public enum Unit {
     FEET_TO_INCH(1 * 12.0),
-    INCH_TO_FEET(1 / 12.0);
+    INCH_TO_FEET(1 / 12.0),
+    FEET_TO_YARD(1 / 3.0);
 
     public final double lengthValue;
 
