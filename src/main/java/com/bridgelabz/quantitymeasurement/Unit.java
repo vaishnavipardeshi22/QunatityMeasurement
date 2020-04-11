1f427d2590a9fd10ed6da235977931cdcdffc8d1
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-04-11 15:42:33 +0530

    ADDED : Enum constant for millilitre to litre conversion

--- src/main/java/com/bridgelabz/quantitymeasurement/Unit.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/Unit.java
@@ -6,7 +6,8 @@ public enum Unit {
     FEET_TO_YARD(1 / 3.0), YARD_TO_FEET(1 * 3.0),
     INCH_TO_YARD(1 / 36.0), YARD_TO_INCH(1 * 36.0),
     INCH_TO_CENTIMETER(1 * 2.54), CENTIMETER_TO_INCH(1 / 2.54),
-    GALLON_TO_LITRES(1 * 3.78), LITRE_TO_MILLILITRE(1 * 1000.0);
+    GALLON_TO_LITRES(1 * 3.78),
+    LITRE_TO_MILLILITRE(1 * 1000.0), MILLILITRE_TO_LITRE(1 / 1000.0);
 
     public final double lengthValue;
 
