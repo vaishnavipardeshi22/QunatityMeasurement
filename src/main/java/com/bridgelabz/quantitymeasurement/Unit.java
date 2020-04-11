681d3039ee18316d2661e47d2a2fc9905e1c11f3
vaishnavipardeshi <vaishnavipardeshi22@gmail.com>
2020-04-11 12:25:18 +0530

    REFACTOR : Enum entity

--- src/main/java/com/bridgelabz/quantitymeasurement/Unit.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/Unit.java
@@ -4,8 +4,8 @@ public enum Unit {
     FEET_TO_INCH(1 * 12.0),
     INCH_TO_FEET(1 / 12.0),
     FEET_TO_YARD(1 / 3.0),
-    INCH_TO_YARD(1 / 36),
-    YARD_TO_INCH(1 * 36);
+    INCH_TO_YARD(1 / 36.0),
+    YARD_TO_INCH(1 * 36.0);
 
     public final double lengthValue;
 
